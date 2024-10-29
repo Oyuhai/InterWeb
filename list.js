223a12cbc52ce70c783faa0c35931c097f6c650a
Oyut <oyut.battamir@gmail.com>
2024-10-29 19:06:16 +0800

    reduce ashigblasan

--- list.js
+++ list.js
@@ -1,147 +1,21 @@
-const params = new URLSearchParams(window.location.search)
 
-const mydata = [
-    {
-        "id": 1,
-        "profession": "system_analist",
-        "location": "hud",
-        "duration": "1_3",
-        "company": "ICT",
-        "salary": 1200000,
-        "status": "open"
-    },
-    {
-        "id": 2,
-        "profession": "frontend_developer",
-        "location": "chd",
-        "duration": "4_6",
-        "company": "Itools",
-        "salary": 900000,
-        "status": "closed"
-    },
-    {
-        "id": 3,
-        "profession": "backend_developer",
-        "location": "bzd",
-        "duration": "7_9",
-        "company": "Itheme",
-        "salary": 1400000,
-        "status": "open"
-    },
-    {
-        "id": 4,
-        "profession": "frontend_developer",
-        "location": "hud",
-        "duration": "1_3",
-        "company": "ICT",
-        "salary": 800000,
-        "status": "closed"
-    },
-    {
-        "id": 5,
-        "profession": "backend_developer",
-        "location": "chd",
-        "duration": "4_6",
-        "company": "Itools",
-        "salary": 1300000,
-        "status": "open"
-    },
-    {
-        "id": 6,
-        "profession": "system_analist",
-        "location": "bzd",
-        "duration": "7_9",
-        "company": "Itheme",
-        "salary": 1500000,
-        "status": "closed"
-    },
-    {
-        "id": 7,
-        "profession": "frontend_developer",
-        "location": "hud",
-        "duration": "4_6",
-        "company": "ICT",
-        "salary": 1100000,
-        "status": "open"
-    },
-    {
-        "id": 8,
-        "profession": "backend_developer",
-        "location": "chd",
-        "duration": "1_3",
-        "company": "Itools",
-        "salary": 700000,
-        "status": "closed"
-    },
-    {
-        "id": 9,
-        "profession": "system_analist",
-        "location": "bzd",
-        "duration": "4_6",
-        "company": "Itheme",
-        "salary": 950000,
-        "status": "open"
-    },
-    {
-        "id": 10,
-        "profession": "frontend_developer",
-        "location": "hud",
-        "duration": "7_9",
-        "company": "ICT",
-        "salary": 1250000,
-        "status": "closed"
-    },
-    {
-        "id": 11,
-        "profession": "system_analist",
-        "location": "chd",
-        "duration": "1_3",
-        "company": "Itools",
-        "salary": 1350000,
-        "status": "open"
-    },
-    {
-        "id": 12,
-        "profession": "backend_developer",
-        "location": "bzd",
-        "duration": "4_6",
-        "company": "Itheme",
-        "salary": 1450000,
-        "status": "closed"
-    },
-    {
-        "id": 13,
-        "profession": "system_analist",
-        "location": "hud",
-        "duration": "7_9",
-        "company": "ICT",
-        "salary": 500000,
-        "status": "open"
-    },
-    {
-        "id": 14,
-        "profession": "frontend_developer",
-        "location": "chd",
-        "duration": "1_3",
-        "company": "Itools",
-        "salary": 750000,
-        "status": "closed"
-    },
-    {
-        "id": 15,
-        "profession": "backend_developer",
-        "location": "bzd",
-        "duration": "7_9",
-        "company": "Itheme",
-        "salary": 1350000,
-        "status": "open"
-    }
-]
+const params = new URLSearchParams(window.location.search)
 
 const professionDictionary = {
     backend_developer: "Backend Хөгжүүлэгч",
     system_analist: "Систем шинжээч",
     frontend_developer: "Frontend Хөгжүүлэгч"
+}
+
+const locationDictionary = {
+    bzd: "Баянзүрх дүүрэг",
+    chd: "Чингэлтэй дүүрэг",
+    hud: "Хан-Уул дүүрэг"
+}
+
+const statusDictionary = {
+    open: "Нээлттэй",
+    closed: "Хаагдсан"
 }
 
 
@@ -164,10 +38,13 @@ function getFilter() {
             filter.push({ key, value: values[0] })
         }
     }
+    console.log('====>getfilter', filter)
     return filter
 }
 
-function filterData(filters) {
+async function filterData(filters) {
+    const mydata = await fetch('/mydata.json')
+        .then((response) => response.json())
     const filteredData = mydata.filter(e => {
         const result = filters.map(({ key, value }) => e[key] == value)
         return result.every(e => e == true)
@@ -175,19 +52,18 @@ function filterData(filters) {
     return filteredData
 }
 
-function Render() {
-    let data = ``
+async function Render() {
     const filters = getFilter()
-    const filteredData = filterData(filters)
-    filteredData.forEach(e => {
-        data += `<li>
-                    <h4>${professionDictionary[e.profession]}</h4>
-                    <p>Компани: ${e.company}, Байршил:${e.location}</p>
-                    <p>${e.salary}₮</p>
-                    <p>${e.status}</p>
+    const filteredData = await filterData(filters)
+    const data = filteredData.reduce((p, c) => {
+        return p += `<li>
+                    <h4>${professionDictionary[c.profession]}</h4>
+                    <p>Компани: ${c.company}, Байршил:${locationDictionary[c.location]}</p>
+                    <p>${c.salary}₮</p>
+                    <p>${statusDictionary[c.status]}</p>
                     <button>Дэлгэрэнгүй</button>
                 </li>`
-    })
+    }, '')
     const el = document.getElementById("zar")
     el.innerHTML = data
 }
