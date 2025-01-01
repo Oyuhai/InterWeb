bc4bf27c0094f5feb47a264dc8ed1a36fa01b4d1
oyuhaii <oyut.bbb@gmail.com>
2025-01-01 20:49:56 +0800

    homepage suuld nemegdsen zaruud json-s garj irdg blsn

--- home.js
+++ home.js
@@ -7,10 +7,10 @@ const professionDictionary = {
     system_admin: "Системийн админ"
 }
 
-async function Init() {
-    const mydata = await fetch('/mydata.json')
-        .then((response) => response.json()) //json datagaa irtel huleene
+const mydata = await fetch('/mydata.json')
+    .then((response) => response.json()) //json datagaa irtel huleene
 
+async function ActiveZarHAruulah() {
     const companyDictionary = {} //company ner=key object uusgene
 
     mydata.forEach(zar => {
@@ -32,17 +32,40 @@ async function Init() {
                 return p + `<li>${professionDictionary[c.profession]}</li>`;
             else return p
         }, '')}
-                        </ul>
-                    </article>`
+                        </ul >
+                    </article > `
         return p += companyHtml
     }, '')
 
-    const b = document.getElementById("test")//HTML dotroosoo id-r in renderlene
+    const b = document.getElementById("companyHaruulah")//HTML dotroosoo id-r in renderlene
     b.innerHTML = homePageCompany
-
-
 }
 
-Init()
+// niitlegdsn ognoogoor in buurah daraalald oruulna.
+const recentItems = mydata
+    .filter(item => item.datePublished) // buh zard date published bga esehiig harna
+    .sort((a, b) => new Date(b.datePublished) - new Date(a.datePublished))
+    .slice(0, 5); //top 5g in avna
+
+
+const listContainer = document.getElementById('zarHaruulah');
+listContainer.innerHTML = recentItems.reduce((html, item) => {
+    return html + `
+        <li class="ad-container">
+            <div>
+                <h4>${item.profession}</h4>
+                <p>Компани: ${item.company}, Байршил: ${item.location}</p>
+                <p>${item.salary.toLocaleString()}₮</p>
+            </div>
+            <div class="ad-container-bottom">
+                <p>${item.status === 'open' ? 'Нээлттэй' : 'Хаалттай'}</p>
+                <button>Дэлгэрэнгүй</button>
+            </div>
+        </li>`;
+}, '');
+
+
+ActiveZarHAruulah()
+SuuldOruulsanZarHaruulah()
 
 
