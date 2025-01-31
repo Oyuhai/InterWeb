0309eb0d6a17817432789f47546340592dd420b9
Altjin09 <151597260+Altjin09@users.noreply.github.com>
2025-01-31 15:00:21 +0800

    git

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "postgre",
+    host: "localhost",
+    database: "webinter",
+    password: "22B1NUM2341",
+    port: 5432, // Default PostgreSQL port
+});
+
+module.exports = pool;
